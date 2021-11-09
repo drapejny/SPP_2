2b9117d30b37982335ea9e15adcac1a1a6e9f6ec
Slizh Anton <antoshka-51762@yandex.by>
2021-11-09 10:28:34 +0300

    create Faker class

---
+++ Faker/Faker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class Faker : IFaker
+    {
+        public object Create(Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
