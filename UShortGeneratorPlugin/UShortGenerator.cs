8a6743fe0aadea13ea90a6b1e8fc8825401d3ffd
Slizh Anton <antoshka-51762@yandex.by>
2021-11-09 13:33:38 +0300

    Create unsigned short generator plugin

---
+++ UShortGeneratorPlugin/UShortGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Faker.Generators;
+
+namespace UShortGeneratorPlugin
+{
+    public class UShortGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            return (ushort)(new Random().Next(ushort.MinValue, ushort.MaxValue));
+        }
+
+        public Type GeneratorType()
+        {
+            return typeof(uint);
+        }
+    }
+}
