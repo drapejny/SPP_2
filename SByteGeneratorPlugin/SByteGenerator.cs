aa95d5ea44e1cc8193f01c179bba63550e952ee6
Slizh Anton <antoshka-51762@yandex.by>
2021-11-09 13:34:50 +0300

    Create signed byte generator plugin

---
+++ SByteGeneratorPlugin/SByteGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Faker.Generators;
+
+namespace SByteGeneratorPlugin
+{
+    public class SByteGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            return (sbyte)(new Random().Next(sbyte.MinValue, sbyte.MaxValue));
+        }
+
+        public Type GeneratorType()
+        {
+            return typeof(sbyte);
+        }
+    }
+}
