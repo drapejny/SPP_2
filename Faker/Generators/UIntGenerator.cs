a27915bd697686da0bac2a16e8e43e90131bda60
Slizh Anton <antoshka-51762@yandex.by>
2021-11-09 20:15:46 +0300

    Add more generators

---
+++ Faker/Generators/UIntGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Generators
+{
+    public class UIntGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            Random random = new Random();
+            uint thirtyBits = (uint)random.Next(1 << 30);
+            uint twoBits = (uint)random.Next(1 << 2);
+            uint result = (thirtyBits << 2) | twoBits;
+            return result;
+        }
+
+        public Type GeneratorType()
+        {
+            return typeof(uint);
+        }
+    }
+}
---
+++ Faker/Generators/ULongGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Generators
+{
+    public class ULongGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            UIntGenerator uIntGenerator = new UIntGenerator();
+            uint first = (uint) uIntGenerator.Generate();
+            uint second = (uint)uIntGenerator.Generate();
+            ulong result = (ulong)first;
+            result <<= 32;
+            result = result | ((ulong)second);
+            return result;
+        }
+
+        public Type GeneratorType()
+        {
+            return typeof(ulong);
+        }
+    }
+}
